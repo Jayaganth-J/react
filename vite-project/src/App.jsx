e41fd8fc4a5d0c58378b2fbea49ff5f7d8408466
Jayaganth-J <jayaganthjagan7@gmail.com>
2025-02-15 22:47:54 -1200

    assessment-1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div class="color">
+    <h1 class="jay">name:jayaganth</h1>
+    </div>
+       
+    </>
+  )
+}
+
+export default App
