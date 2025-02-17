3df5aaea904a452ba1e0a2e512f571983ff361f2
Jayaganth-J <jayaganthjagan7@gmail.com>
2025-02-17 02:45:15 -1200

    rohin

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,10 +2,32 @@ import './App.css'
 
 function App() {
 
+
   return (
     <>
+    {/* <div class="background">
     <div class="color">
-    <h1 class="jay">name:jayaganth</h1>
+    <div class="jay">
+    <h1 class="">name:jayaganth</h1>
+    <h1 class="">Age:19</h1>
+    <h1 class="">Dept:M.Tech Computer Science Engineering </h1>
+    </div>
+    </div>
+    </div> */}
+<div class="body">
+    <div class="container">
+      <center> 
+      <h1>name:jayaganth</h1>
+      <h2>Age:19</h2>
+      <h2>Dept:M.Tech Computer Science Engineering</h2>
+      </center>
+    </div>
+    <div class="button"><ul>
+    <li><button>Click1</button></li>
+    <li><button>Click2</button></li>
+    <li><button>Click3</button></li>
+    </ul>
+    </div>
     </div>
        
     </>
